09bf747207e5ccedf5c0fbc20a88b2e577f31d69
Md Sajib Pramanic <118989261+SAJIB3489@users.noreply.github.com>
2023-03-14 23:32:55 +0200

    Add files via upload

---
+++ Functions 01 - Using an existing function.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdbool.h>
+
+bool isPrime(int p);
+
+void printPrimes(int lim) {
+	for (int i = 2; i <= lim; i++) {
+		if (isPrime(i)) {
+			printf("%d\t", i);
+		}
+	}
+	printf("\n");
+}
+bool isPrime(int p) {
+	for (int u = 2; u < p; u++) {
+		if (p % u == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+int main(int argc, char** argv) {
+	printPrimes(100);
+}
